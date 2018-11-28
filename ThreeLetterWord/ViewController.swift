7cc3f7588f1e408de9fdc3e96525fff6a68224c1
BRANDON GOMEZ <rl230566@rlas-macair-19.local>
2018-11-28 11:14:19 -0600

    Initial Commit

---
+++ ThreeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreeLetterWord
+//
+//  Created by BRANDON GOMEZ on 11/28/18.
+//  Copyright © 2018 BRANDON GOMEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
